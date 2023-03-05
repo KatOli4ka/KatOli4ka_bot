62654ff4bb34a9b677f2631b506467f1f336be95
KatOli4ka <katoli4ka87@mail.ru>
2023-03-05 19:52:14 +0500

    Initial commit

---
+++ src/main/java/pro/sky/katoli4ka_bot/KatOli4kaBotApplication.java
@@ -0,0 +1,13 @@
+package pro.sky.katoli4ka_bot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KatOli4kaBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KatOli4kaBotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pro/sky/katoli4ka_bot/KatOli4kaBotApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.katoli4ka_bot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KatOli4kaBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
